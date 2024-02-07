6b14821a9a2d6d1cb38f65e82563bf78e2b37194
RACLesty <ztos@msn.com>
2024-02-07 15:55:45 -0500

    Select/Check Cards

--- script.js
+++ script.js
@@ -7,7 +7,14 @@ const btnEl = document.querySelector('button')
 const symbols = ['X', 'O', '$', '*', '+', '='];
 const symbolsList = [...symbols, ...symbols]
 
+let selectedCards = [];
+let move = false;
+
+let matchedCount = 0;
+
 runGame();
+
+gameCardsEl.addEventListener('click', selectCard);
 
 function runGame() {
     shuffleCards()
@@ -21,8 +28,37 @@ function shuffleCards() {
     })
 }
 
-function selectCard() {
+function selectCard(event) {
+    const clickedCard = event.target;
+    if (
+    !clickedCard.classList.contains('card') || 
+    move || 
+    selectedCards.includes(clickedCard) || 
+    clickedCard.classList.contains('matched')
+    ) return;
+    flipCard(clickedCard)
+    selectedCards.push(clickedCard);
+    if (selectedCards.length ===2) {
+        move = true
+        checkMatch()
+    }
+}
 
+function flipCard(card) {
+    card.classList.add('flipped')
+}
+
+function checkMatch(){
+    const [card1, card2] = selectedCards;
+    const symbol1 = card1.getAttribute('data-symbol');
+    const symbol2 = card2.getAttribute('data-symbol');
+    if (symbol1 === symbol2) {
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+        matchedCount += 2;
+    }
+    move = false
+    selectedCards = []
 }
 
 function startTimer() {
