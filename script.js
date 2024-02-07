5ffa26278ba94251c2b5a7166056203329b7218d
RACLesty <ztos@msn.com>
2024-02-07 14:08:23 -0500

    Function's Keleton

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ const cardEl = document.querySelectorAll('.card');
 const outcomeMsgEl = document.querySelector('#outcome-msg');
 const timerEl = document.querySelector('#timer')
 const btnEl = document.querySelector('button')
+
+
+runGame()
+
+function runGame() {
+
+}
+
+function shuffleCards() {
+
+}
+
+function selectCard() {
+
+}
+
+function startTimer() {
+
+}
+
+function restartGame() {
+    
+}
