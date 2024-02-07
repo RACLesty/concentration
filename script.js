3a4fdb17026176ac6bf59ec97326856a0668fb53
RACLesty <ztos@msn.com>
2024-02-07 13:58:07 -0500

    Reference Setup

---
+++ script.js
@@ -0,0 +1,5 @@
+const gameCardsEl = document.querySelector('#cards-wrapper');
+const cardEl = document.querySelectorAll('.card');
+const outcomeMsgEl = document.querySelector('#outcome-msg');
+const timerEl = document.querySelector('#timer')
+const btnEl = document.querySelector('button')
